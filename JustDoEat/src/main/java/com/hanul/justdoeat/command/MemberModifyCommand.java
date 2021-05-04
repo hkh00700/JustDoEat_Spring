c59a25f11caa4a345b99d82da4330f5af6901f8a
201-05\user <5745531@naver.com>
2021-05-04 15:23:30 +0900

    회원정보 수정

---
+++ JustDoEat/src/main/java/com/hanul/justdoeat/command/MemberModifyCommand.java
@@ -0,0 +1,29 @@
+package com.hanul.justdoeat.command;
+
+import org.springframework.ui.Model;
+
+import com.hanul.justdoeat.dao.MemberDAO;
+
+public class MemberModifyCommand implements MemberCommand {
+
+	@Override
+	public void execute(Model model) {
+		String m_pw = (String) model.asMap().get("m_pw");
+		String m_name = (String) model.asMap().get("m_name");
+		String m_phone = (String) model.asMap().get("m_phone");
+		String m_gender = (String) model.asMap().get("m_gender");
+		String m_nikname = (String) model.asMap().get("m_nikname");
+		String m_address1 = (String) model.asMap().get("m_address1");
+		String m_address2 = (String) model.asMap().get("m_address2");
+		String m_email = (String) model.asMap().get("m_email");
+		
+		MemberDAO dao = new MemberDAO();
+		
+		int state = dao.memberModify(m_pw, m_name, m_phone, m_gender, m_nikname, m_address1, m_address2, m_email);
+		
+		model.addAttribute("memberModify", String.valueOf(state));
+		
+	}
+	
+
+}
--- JustDoEat/src/main/java/com/hanul/justdoeat/controller/MemberController.java
+++ JustDoEat/src/main/java/com/hanul/justdoeat/controller/MemberController.java
@@ -8,19 +8,63 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hanul.justdoeat.command.JoinCommand;
 import com.hanul.justdoeat.command.KakaoLoginCommand;
 import com.hanul.justdoeat.command.LoginCommand;
 import com.hanul.justdoeat.command.MemberCommand;
+import com.hanul.justdoeat.command.MemberModifyCommand;
 
 @Controller
 public class MemberController {
 	
 	MemberCommand command;
 	
+	@RequestMapping(value="/memberModify", method = {RequestMethod.GET, RequestMethod.POST})	
+	public String memberModify(HttpServletRequest req, Model model) {
+		
+		System.out.println("memberModify들어왔다");
+ 
+		// 클라이언트(안드로이드)에서 보낸 데이터 받기
+				String m_pw = (String) req.getParameter("m_pw");
+				String m_name = (String) req.getParameter("m_name");
+				String m_phone = (String) req.getParameter("m_phone");
+				String m_gender = (String) req.getParameter("m_gender");
+				String m_nikname = (String) req.getParameter("m_nikname");
+				String m_address1 = (String) req.getParameter("m_address1");
+				String m_address2 = (String) req.getParameter("m_address2");
+				String m_email = (String) req.getParameter("m_email");
+				
+				
+				System.out.println(m_pw);
+				System.out.println(m_name);
+				System.out.println(m_phone);
+				System.out.println(m_gender);
+				System.out.println(m_nikname);
+				System.out.println(m_address1);
+				System.out.println(m_address2);
+				System.out.println(m_email);
+				
+				// 받은 데이터 model에 넣기
+				model.addAttribute("m_pw", m_pw);
+				model.addAttribute("m_name", m_name);
+				model.addAttribute("m_phone", m_phone);
+				model.addAttribute("m_gender", m_gender);
+				model.addAttribute("m_nikname", m_nikname);
+				model.addAttribute("m_address1", m_address1);
+				model.addAttribute("m_address2", m_address2);
+				model.addAttribute("m_email", m_email);
+				
+				// command 만들기
+				command = new MemberModifyCommand();
+				command.execute(model);
+				
+				return "memberModify";
+	}
+	
 	@RequestMapping(value="/memberLogin", method = {RequestMethod.GET, RequestMethod.POST})	
-	public String anLogin(HttpServletRequest req, Model model) {
+	public String Login(HttpServletRequest req, Model model) {
 		System.out.println("memberLogin들어왔다");
  
 		try {
