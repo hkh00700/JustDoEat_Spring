9790362276f822e30ccab94e71a2bfaed734cba9
201-05\user <5745531@naver.com>
2021-05-14 11:49:40 +0900

    지도

---
+++ JustDoEat/src/main/java/com/hanul/justdoeat/dto/RestrantDTO.java
@@ -0,0 +1,29 @@
+package com.hanul.justdoeat.dto;
+
+public class RestrantDTO {
+	public String restrant;
+	public float r_latitude;
+	public float r_hardness;
+	
+	public String getRestrant() {
+		return restrant;
+	}
+	public void setRestrant(String restrant) {
+		this.restrant = restrant;
+	}
+	public float getR_latitude() {
+		return r_latitude;
+	}
+	public void setR_latitude(float r_latitude) {
+		this.r_latitude = r_latitude;
+	}
+	public float getR_hardness() {
+		return r_hardness;
+	}
+	public void setR_hardness(float r_hardness) {
+		this.r_hardness = r_hardness;
+	}
+	
+	
+
+}
